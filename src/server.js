5c05762ab53b92dd60f7952f5e0d011f2bedf63c
phongvilaitoto <phongvilaitoto@gmail.com>
2019-12-11 13:03:12 +0700

    pp

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const http = require('http')
 const app = require('./app')
-
-app.get('*', (req, res) => {
-    // none www
-    // if (req.headers.host.match(/^www\./)) {
-    //     res.redirect({ "Location": "https://" + req.headers['host'] + req.url }, 301 )
-    // }
-    res.render('index.ejs')
-})
+//
+// app.get('*', (req, res) => {
+//     // none www
+//     // if (req.headers.host.match(/^www\./)) {
+//     //     res.redirect({ "Location": "https://" + req.headers['host'] + req.url }, 301 )
+//     // }
+//     res.render('index.ejs')
+// })
 
 http.createServer(app).listen(80, () => {
     console.log('server has been created')
